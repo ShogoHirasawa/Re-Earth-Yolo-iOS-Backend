1a88eec2b3bd00dd5773db557c937ee05ef9da07
Shogo Hirasawa <origami.no.1@gmail.com>
2024-07-07 00:55:36 -0400

    Add files via upload

--- models.py
+++ models.py
@@ -1,11 +1,38 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, Column, String, Enum, DateTime, Numeric, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+from datetime import datetime
+import uuid
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class VehicleData(db.Model):
+class VehicleData(Base):
     __tablename__ = 'vehicle_data'
-    uuid = db.Column(db.String(36), primary_key=True)
-    vehicle_type = db.Column(db.Enum('car', 'bus', 'truck','motorcycle','bicycle','person','truck'))
-    timestamp = db.Column(db.DateTime)##ISO 8601形式でデータを格納するためdb.DateTimeは使わずに文字列で格納
-    latitude = db.Column(db.Numeric(10, 7))
-    longitude = db.Column(db.Numeric(10, 7))
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    vehicle_type = Column(Enum('car', 'bus', 'truck', 'motorcycle', 'bicycle', 'person'), nullable=False)
+    timestamp = Column(DateTime, nullable=False)
+    latitude = Column(Numeric(10, 7), nullable=False)
+    longitude = Column(Numeric(10, 7), nullable=False)
+    device_records = relationship('DeviceVehicle', back_populates='vehicle')
+
+    def __repr__(self):
+        return f"<VehicleData(id='{self.id}', vehicle_type='{self.vehicle_type}', timestamp='{self.timestamp}', latitude={self.latitude}, longitude={self.longitude})>"
+
+class DeviceInfo(Base):
+    __tablename__ = 'device_info'
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = Column(String(36), nullable=False)
+    device_id = Column(String(36), nullable=False)
+    vehicle_records = relationship('DeviceVehicle', back_populates='device')
+
+    def __repr__(self):
+        return f"<DeviceInfo(id='{self.id}', name='{self.name}', device_id='{self.device_id}')>"
+
+class DeviceVehicle(Base):
+    __tablename__ = 'device_vehicle'
+    device_id = Column(String(36), ForeignKey('device_info.id'), primary_key=True)
+    vehicle_id = Column(String(36), ForeignKey('vehicle_data.id'), primary_key=True)
+    device = relationship('DeviceInfo', back_populates='vehicle_records')
+    vehicle = relationship('VehicleData', back_populates='device_records')
+
+    def __repr__(self):
+        return f"<DeviceVehicle(device_id='{self.device_id}', vehicle_id='{self.vehicle_id}')>"
